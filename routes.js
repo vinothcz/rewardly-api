5a9e2d0d3b8eeb81556d40e1bb5922d6b3213a6b
Mustafa Saify <saifym@advisory.com>
2016-02-11 01:00:36 +0530

    heroku deployment test

--- routes.js
+++ routes.js
@@ -5,19 +5,19 @@ var user = require('./Controllers/User');
 var feed = require('./Controllers/Feed');
 var reward = require('./Controllers/Reward');
 
-router.route('/api/users')
+router.route('/rewardly/api/users')
   .get(user.GetUsers);
 
-router.route('/api/users')
+router.route('/rewardly/api/users')
   .post(user.SaveUsers);
 
-router.route('/api/feeds')
+router.route('/rewardly/api/feeds')
   .get(reward.GetAllRewards);
 
-router.route('/api/rewards')
+router.route('/rewardly/api/rewards')
   .get(reward.GetAllRewards);
 
-router.route('/api/rewards')
+router.route('/rewardly/api/rewards')
   .post(reward.SaveRewards);
 
 module.exports = router;
